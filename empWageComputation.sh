528eca7799e448af47b216bf7447827418a7b91f
Sam K Varghese <ksam071@gmail.com>
2022-11-30 22:56:26 +0530

    5 commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,32 +10,32 @@ fi
 
 
 
-
+total_days=20
 wage_per_hour=20
 read -p "Enter 1 For Full time, 2 for Half time " total_working_hours
 case $total_working_hours in
 
 1 )
 working_hours=8
-total_wage=$(( wage_per_hour * working_hours ))
+monthly_wage=$(( wage_per_hour * working_hours * total_days ))
 echo " Working hours: $working_hours "
-echo " Total Wage is : $total_wage "
+echo " Monthly Wage is : $monthly_wage "
 
 ;;
 
 2 )
 working_hours=4
-total_wage=$(( wage_per_hour * working_hours ))
+monthly_wage=$(( wage_per_hour * working_hours * total_days ))
 echo " Working hours: $working_hours "
-echo " Total Wage is : $total_wage "
+echo " Monthly Total Wage is : $monthly_wage "
 
 ;;
 
 * )
 working_hours=0
-total_wage=$(( wage_per_hour * working_hours ))
+monthly_wage=$(( wage_per_hour * working_hours * total_days ))
 echo " Working hours: $working_hours "
-echo " Total Wage is : $total_wage "
+echo " Monthly Total Wage is : $monthly_wage "
 
 ;;
 
