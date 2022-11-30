f2a8dd4b34ad4a9fd269866f12a199a7bb157807
Sam K Varghese <ksam071@gmail.com>
2022-11-30 22:02:33 +0530

    THird commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,3 +7,19 @@ echo " Employee is Present "
 else 
 echo " Employee is absent "
 fi
+
+
+
+
+wage_per_hour=20
+read -p "Enter your working hours either 8 or 4 " working_hours
+if [ $working_hours == 8 ]; then
+total_wage=$(( wage_per_hour * working_hours ))
+echo " Total Wage is : $total_wage "
+elif [[ $working_hours -lt 8 && $working_hours -gt 3 ]]; then
+total_wage=$(( wage_per_hour * working_hours ))
+echo " Total Wage is : $total_wage "
+else
+echo " Total Wage is Zero "
+fi
+	
