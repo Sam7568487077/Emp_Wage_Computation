ff8c60345de3a0b91af47ed63d13cfbc1b20c82d
Sam K Varghese <ksam071@gmail.com>
2022-11-30 21:07:32 +0530

    Second Commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo " Hi Employee here you are going to get your salary computation "
+attendence=$(( RANDOM%2 ))
 
+if [[ $attendance == 1 ]]; then
+echo " Employee is Present "
+else 
+echo " Employee is absent "
+fi
